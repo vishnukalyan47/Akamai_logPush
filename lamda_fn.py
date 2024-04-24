09ac9104a37adc5ca77ea4583ab9d6b080a48af2
Vishnu Kalyan Ledalla <42991012+vishnukalyan47@users.noreply.github.com>
2024-04-25 00:51:33 +0530

    Working AWS Lamda Fun lamda_fn.py

--- lamda_fn.py
+++ lamda_fn.py
@@ -1,23 +1,23 @@
 import json
-import boto3
-from botocore.exceptions import ClientError
-from decimal import Decimal
 
 log_path = '/aklogs'
-test_path = '/apistatus'
+status_test = '/apistatus'
+test_path = '/test-path'
 
 def lambda_handler(event, context):
-    print('Request event: ', event)
+    # print('Request event: ', event)
+    test_bodyy = {"event":event}
     response = None
 
     try:
         http_method = event.get('httpMethod')
         path = event.get('path')
-
         if http_method == 'POST' and path == log_path:
-            response = kibana_call(json.loads(event['body']))
+            response = kibana_call(200, json.loads(event['body']))
         elif http_method == 'GET' and path == test_path:
-            response = "Test Vishnu"
+            response = kibana_call(200, test_bodyy)
+        elif http_method == 'GET' and path == status_test:
+            response = kibana_call(200,"Service is up and running - Vishnu")
 
     except Exception as e:
         print('Error:', e)
@@ -26,15 +26,11 @@ def lambda_handler(event, context):
     return response
 
 # Print Response, can add Kibana api here.
-def kibana_call(request_body):
-    try:
-        body = {
-            'Operation': 'SAVE',
-            'Message': 'SUCCESS',
-            'Item': request_body
-        }
-        return (200, body)
-    
-    except ClientError as e:
-        print('Error:', e)
-        return (400, e.response['Error']['Message'])
+def kibana_call(status_code, request_body):
+    return {
+        'statusCode' : status_code,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps(request_body)
+    }
